ff32de51b75bd6d8438d5ee0c2432cf75ce4a1a9
Leo Bartholomeu <96806061+WonderfulWan8@users.noreply.github.com>
2025-12-29 09:23:25 +0800

    feat: add AddFiveStorage contract extending SimpleStorage

---
+++ src/03_Solidity_Basics/02_StorageFactory/07_Inheritance_In_Solidity.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+// pragma solidity 0.8.19;
+pragma solidity >=0.8.18 <0.9.0;
+
+import {SimpleStorage} from "./01_SimpleStorage.sol";
+
+contract AddFiveStorage is SimpleStorage {
+    function store(uint256 _newNumber) public override {
+        myFavoriteNumber = _newNumber + 5;
+    }
+}
